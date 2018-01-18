7418076b7530876b1684357ad3d456a9e75d05e9
Chaudhary Himanshu Raj <chaudharyhimanshuraj@Chaudharys-Mac.local>
2018-01-19 00:22:22 +0530

    First Commit : TableView Working, Json retrieved.

--- CallbacksAndMultithreading/ViewController.swift
+++ CallbacksAndMultithreading/ViewController.swift
@@ -10,16 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var userNames = ["Name1", "Name2", "Name3", "Name4", "Name5", "Name6", "Name7", "Name8", "Name9", "Name10"]
+    // var userNames = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        getUserInfo()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func getUserInfo() {
+        guard let path = Bundle.main.path(forResource: "UserDetails", ofType: "json") else { return }
+        let url = URL(fileURLWithPath: path)
+        do {
+            let data = try Data(contentsOf: url)
+            print("Data Recieved is : \(data)")
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+            print("Json retrieved is : \(json)")
+            
+        }catch {
+            print(error)
+        }
+    }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.userNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = userNames[indexPath.row]
+        return cell
+    }
 }
 
